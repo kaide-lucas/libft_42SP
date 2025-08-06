eb978cb6dc4d04d11b69ac8e565e3eb2ca669ca0
kaide-lucas <kaidelucas35@gmail.com>
2025-08-05 22:20:55 -0300

    putchar

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,11 +6,31 @@
 /*   By: kaide <kaide@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/05 18:53:46 by kaide             #+#    #+#             */
-/*   Updated: 2025/08/05 18:58:44 by kaide            ###   ########.fr       */
+/*   Updated: 2025/08/05 22:18:55 by kaide            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-size_t ft_strlcat(char *dest, const char *src, size_t dstsize)
+size_t	ft_strlcat(char *dest, const char *src, size_t dstsize)
 {
-	
+	size_t	dest_len;
+	size_t	src_len;
+	size_t	i;
+	size_t	j;
+
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	if (dstsize <= dest_len)
+	{
+		return (dstsize + src_len);
+	}
+	i = dest_len;
+	j = 0;
+	while (src[j] != '\0' && i < dstsize - 1)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest_len + src_len);
 }
