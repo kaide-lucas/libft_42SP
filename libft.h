b4bd64301af6a452be01cf81b78b5adfe21c45e1
kaide-lucas <76270182+kaide-lucas@users.noreply.github.com>
2025-08-12 19:26:49 +0000

    Atualizão

--- libft.h
+++ libft.h
@@ -40,5 +40,6 @@ void	*ft_calloc(size_t nmemb, size_t size);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
+char	*ft_strrchr(const char *s, int c);
 
 #endif
