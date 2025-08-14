733ee9e7c89205a4bb959410f5d9ac50283abca9
kaide-lucas <76270182+kaide-lucas@users.noreply.github.com>
2025-08-14 21:36:41 +0000

    upload

---
+++ ft_strjoin.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-08-13 13:25:13 by codespace         #+#    #+#             */
+/*   Updated: 2025-08-13 13:25:13 by codespace        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*res;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	res = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) +1) * sizeof(char));
+	if (!res)
+		return (NULL);
+	while (s1[i])
+	{
+		res [j++] = s1[i++];
+	}
+	i = 0;
+	while (s2[i])
+	{
+		res[j++] = s2[i];
+	}
+	res[j] = 0;
+	return (res);
+}
